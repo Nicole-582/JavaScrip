9f6b11767c5af7c0be55c67f1c594f1c56ab98c4
Nicole-582 <shugulinicole2008@gmail.com>
2025-07-24 09:28:50 -0500

    Primer commit

---
+++ Calculadora.js
@@ -0,0 +1,36 @@
+'use strict';
+const pantalla = document.getElementById('pantalla');
+let operacion = '';
+
+function agregar(valor) {
+  if (pantalla.textContent === '0' || pantalla.textContent === 'Error') {
+    operacion = valor;
+  } else {
+    operacion += valor;
+  }
+  pantalla.textContent = operacion;
+}
+
+function limpiar() {
+  operacion = '';
+  pantalla.textContent = '0';
+}
+
+function retroceder() {
+  operacion = operacion.slice(0, -1);
+  pantalla.textContent = operacion || '0';
+}
+
+function calcular() {
+  try {
+    let resultado = eval(operacion.replace('%', '/100'));
+    pantalla.textContent = resultado;
+    operacion = resultado.toString();
+  } catch {
+    pantalla.textContent = 'Error';
+    operacion = '';
+  }
+}
+
+
+module.exports=calcular;
